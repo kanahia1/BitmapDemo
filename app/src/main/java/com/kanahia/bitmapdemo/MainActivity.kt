4497968ff5b224194186928e1cdf574f88c877a2
Kanahia <kaushalkanahia@gmail.com>
2024-08-09 22:24:46 +0530

    Added functionality

--- app/src/main/java/com/kanahia/bitmapdemo/MainActivity.kt
+++ app/src/main/java/com/kanahia/bitmapdemo/MainActivity.kt
@@ -1,19 +1,55 @@
 package com.kanahia.bitmapdemo
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.kanahia.bitmapdemo.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var imageUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val container = binding.save
+        imageUri = createImageUri()
+
+        binding.saveBtn.setOnClickListener {
+            val bitmap = getBitmapFromView(container)
+            storeBitmap(bitmap)
+        }
+    }
+
+    private fun storeBitmap(bitmap: Bitmap) {
+        val outputStream = applicationContext.contentResolver.openOutputStream(imageUri)
+        if (outputStream != null) {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        }
+        outputStream!!.close()
+    }
+
+    fun createImageUri(): Uri {
+        val image = File(applicationContext.filesDir, "camera_photo.png")
+        return FileProvider.getUriForFile(applicationContext, "com.kanahia.bitmapdemo", image)
+    }
+
+    fun getBitmapFromView(view: View): Bitmap {
+        val bitmap = Bitmap.createBitmap(view.height, view.width, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val bg = view.background
+        bg.draw(canvas)
+        view.draw(canvas)
+        return bitmap
     }
 }
